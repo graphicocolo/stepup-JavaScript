c29afe4a8a9fdc2721da70cf514d06fa8eeca12a
高橋礼 <graphicocolo@gmail.com>
2022-12-16 10:52:07 +0900

    for ではなく forEach でイテレートして市区町村データを取得

--- step8/8-5/8-5-3/main.js
+++ step8/8-5/8-5-3/main.js
@@ -7,32 +7,63 @@
 
 // Promise.all を使用、通信を各データファイル1ファイルにつき一度ずつだけにする仕様
 // prefectures のデータを元に cities のデータはイテレート
+// for ではなく forEach でイテレートして市区町村データを取得
 
 // ルートになる要素を取得
 const rootElm = document.getElementById('areaSelector');
 
-// 都道府県の情報を格納する変数
+// 都道府県の情報を格納する変数（グローバル）
 let _prefs;
-// 市区町村の情報を格納する変数
+// 市区町村の情報を格納する変数（グローバル）
 let _cities;
 
 // 初期化メソッド 都道府県・市区町村データ取得
 async function initAreaSelector() {
+
+  // 都道府県初期データ取得
   const prefs = await getPrefs();
   _prefs = prefs;
-  const _prefsCode = _prefs.map(pref => pref.code);
+  // const _prefsCode = _prefs.map(pref => pref.code);
+
   const cityArray = [];
-  for (i = 0;i < _prefsCode.length;i++) {
-    cityArray.push(getCities(_prefsCode[i]));
-  }
-  const [...cities] = await Promise.all(cityArray);
-  _cities = {};
-  for (i = 0;i < _prefsCode.length;i++) {
-    let serial = new String(i + 1);
-    serial = `00${serial}`;
-    _cities[`${serial}`] = cities[i];
+  for (let pref of _prefs) {
+    cityArray.push(getCities(pref.code));
   }
 
+  // 市区町村初期データ取得
+  const [...cities] = await Promise.all(cityArray);
+  _cities = {};
+
+  cities.forEach((city, index) => {
+    let serial = new String(index + 1);
+    serial = `00${serial}`;
+    _cities[`${serial}`] = cities[index];
+  });
+
+  // ↓動かないコード
+  // cities.forEach((city, index) => {
+  //   console.log(`${index}: ${city}`);
+  //   ↓この行怪しい...
+  //   _cities[`${city[index][].prefCode}`] = cities[index];
+  // });
+
+  // ↓動かないコード
+  // let i = 0;
+  // for (let city of cities) {
+    // let serial = city.prefCode;
+    // cities[] に値の入れようがない？
+    // _cities[serial] = cities[i];
+  // }
+
+  // ↓正常に動くコード
+  // for ではなく for of でスマートに記述したい
+  // for (i = 0;i < _prefsCode.length;i++) {
+  //   let serial = new String(i + 1);
+  //   serial = `00${serial}`;
+  //   _cities[`${serial}`] = cities[i];
+  // }
+
+  // 都道府県・市区町村初期データ設定
   await updatePref();
   await updateCity();
 }
