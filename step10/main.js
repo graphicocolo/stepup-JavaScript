e63131698a35bc35af3f081d2fcd6e048376eb9f
高橋礼 <graphicocolo@gmail.com>
2023-01-13 10:47:08 +0900

    正答率の計算ロジックを書く

--- step10/main.js
+++ step10/main.js
@@ -3,6 +3,7 @@ class WordQuiz {
     this.rootElm = rootElm;
     // ゲームのステータス
     this.gameStatus = {};
+    // 各ステータスの初期化
     this.resetGame();
   }
 
@@ -32,6 +33,7 @@ class WordQuiz {
 
   // 同レベル内の問題番号（step1）に対応した設問を表示
   nextStep() {
+    this.addResult();
     if (this.isLastStep()) {
       this.displayResultView();
     } else {
@@ -40,9 +42,47 @@ class WordQuiz {
     }
   }
 
+  // 解答結果と正答のデータの情報追加
+  addResult() {
+    const checkedElm = this.rootElm.querySelector('input[name="choice"]:checked');
+    const answer = checkedElm ? checkedElm.value : '';
+    const currentQuestion = this.quizData[this.gameStatus.level][`step${this.gameStatus.step}`];
+
+    // 配列 results に、question キーと selectedAnswer キーを保持したオブジェクトが追加されていく
+    // 配列内に、オブジェクトが1つずつ追加されていく
+    this.gameStatus.results.push({
+      question: currentQuestion,
+      selectedAnswer: answer
+    });
+
+    console.log(`解答結果：${answer}`);
+    console.log(this.gameStatus.results);
+  }
+
+  // 正答率の計算
+  // ユーザーがラジオボタンを選択しなかった場合、誤答として認識される
+  calcScore() {
+    // 正答数
+    let correctNum = 0;
+    // 問題数
+    const results = this.gameStatus.results;
+
+    for (const result of results) {
+      const selected = result.selectedAnswer;
+      const correct = result.question.answer;
+      if (selected === correct) {
+        correctNum++;
+      }
+    }
+    // Math.floor を通すことで小数点以下が切り捨てられる
+    return Math.floor((correctNum / results.length) * 100);
+  }
+
+  // 問題レベル・設問番号・解答結果の初期化
   resetGame() {
     this.gameStatus.level = null; // 選択されたレベル
     this.gameStatus.step = 1; // 現在表示している設問の番号
+    this.gameStatus.results = []; // プレイヤーの解答結果
   }
 
   // 開始画面の表示、データ表示要素の生成
@@ -113,9 +153,13 @@ class WordQuiz {
   }
 
   // 終了画面の表示
+  // 開始画面に戻るをクリックするたび、問題のレベル・解答結果はリセットされる
   displayResultView() {
+    const score = this.calcScore();
+
     const html = `
-      <p>ゲーム終了</p>
+      <h2>ゲーム終了</h2>
+      <p>正答率：${score}%</p>
       <button class="resetBtn">開始画面に戻る</button>
     `;
 
